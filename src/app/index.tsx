76eb52ae636df2f092fcfd8b0f43247a63c0a8e0
Shirley <shirleyprietocamacho@gmail.com>
2026-09-17 19:07:13 -0500

    Modify greeting and add navigation link
    Updated text to display 'Shirley Prieto' and added a link to the About Us page.

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,13 +1,22 @@
-import { StyleSheet, View } from "react-native";
+import { Link } from "expo-router";
+import { StyleSheet, Text, View } from "react-native";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <text style={styles.text}>Hello, word </text>
+      <Text style={styles.text}>Shirley Prieto</Text>
+      <Link href="/about" style={styles.button}>
+        Go to About Us
+      </Link>
     </View>
   );
 }
 const styles = StyleSheet.create({
+  button: {
+    fontSize: 18,
+    color: "#ebafcd",
+    backgroundColor: "#7a1a72",
+  },
   container: {
     flex: 1,
     justifyContent: "center",
